237b52e294c13005d52f83f1a81b8675a10cc302
eunisae sea <eunisae sea>
2017-08-29 15:58:03 +0200

    sd

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path')
 const app = express()
 
 app.use('/', express.static(path.join(__dirname, 'app')))
+app.get('*' , function(req , res){
+  res.sendFile(path.join(__dirname+'/app/index.html'));
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
